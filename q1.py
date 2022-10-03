d4a18bce37c7a6a9ae63d02b15c648d6d8eaf014
Alexander Evans <kingevans22202@gmail.com>
2022-10-03 14:53:55 -0700

    q1

--- q1.py
+++ q1.py
@@ -1,4 +1,25 @@
+numbers = []
+for i in range(10):
+    numbers.append(int(input()))
 
+
+x = 0
+count = 0
+while(x < 9):
+    
+    if numbers[x]%2 == 0 and numbers[x+1]%2 == 0:
+        x = x+1
+        count = count + 1
+        while (numbers[x]%2 == 0):
+            if x == 9:
+                break
+            x = x+1
+
+    else:
+        x = x+1
+
+print(count)
+# ******************************
 # ******************************
 # Make your Code
 # ******************************
